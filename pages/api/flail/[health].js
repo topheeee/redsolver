ba0589510f486951efa6bcf2920aad6ef003f625
topheeee <tophrhel@gmail.com>
2021-02-22 00:01:09 -0700

    adding flail api route

---
+++ pages/api/flail/[health].js
@@ -0,0 +1,17 @@
+export default (req, res) => {
+  const {
+    query: { health },
+  } = req
+
+  const flail100 = Math.floor(0.20312 * health)
+  const flail150 = Math.floor(0.09375 * health)
+  const flail200 = Math.floor(0.03125 * health)
+
+  res.end(
+    `
+    100 -> ${flail100}
+    150 -> ${flail150}
+    200 -> ${flail200}
+    `
+  )
+}
